42ab384df8ac6d77dce51f9c5d82b2360ddeaf24
funkey7dan <72495653+funkey7dan@users.noreply.github.com>
2021-10-29 18:30:48 +0300

    Commented all

--- client.py
+++ client.py
@@ -11,22 +11,26 @@ s.settimeout(1)
 
 bytes = file.read(BUFFER)
 index = 0
+# while we have chunks left in the file
 while bytes:
+    # send the chunk to the server, while appending it's index
     s.sendto(str(index).encode()+b'_'+bytes,(IP,PORT))
-    #print("sent\n" + str(index)+'_'+ bytes.decode())
-    received_bool = False
+    received_bool = False # flag to know whether the server replies
+    # while we have no answer from the server
     while not received_bool:
+        # try to recieve a datagram from the server
         try:
             data,addr = s.recvfrom(BUFFER)
-            #print("received:\n"+data.decode())
+            # if we received a datagram, check if it's the index of the chunk we send, as an ACK from server
             if data.decode() == str(index):
                 received_bool = True
+                # increment index count and read next chunk
                 index += 1
                 bytes = file.read(BUFFER)
+        # if we didn't receive data from the server, send it again
         except:
             s.sendto(str(index).encode()+b'_'+bytes,(IP,PORT))
-            #print("sent\n"+str(index)+bytes.decode())
-
 file.close()
+#close the socket
 s.close()
 exit(0)
--- server.py
+++ server.py
@@ -6,22 +6,27 @@ BUFFER = 100
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind(('',PORT))
-received_indexes_list = []
+received_indexes_list = []  # list to hold all the indexes we already received
+
+# main script loop
 while True:
-    received_bool = False
+    received_bool = False  # flag to know whether the client sent something
     while not received_bool:
+        # try to recieve a datagram from the server
         try:
             data,addr = s.recvfrom(BUFFER)
+            #split the data using the delimiter to separate the index and the data
             data_decoded = data.decode('utf8').split('_')
             index = data_decoded[0]
             data = data_decoded[1]
+            # if we didn't receive this data before, the index is not in our list, we print the data
             if index not in received_indexes_list:
-                # data_arr = bytearray(data)
-                # del data_arr[0]
                 print(data,end = '')
+                # add the index to the list
                 received_indexes_list.append(index)
             received_bool = True
+            # send the index to the client to notify we got the message
             s.sendto(index.encode(),addr)
-        except Exception as e:
-            print(str(e)+"ERROR")
+        # if no data was received, go to the next iteration
+        except:
             continue
