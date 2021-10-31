c8606a70b1d8856c97db5cb1299c23cd0eb23c51
funkey7dan <72495653+funkey7dan@users.noreply.github.com>
2021-10-31 09:32:47 +0200

    Placed while loop in a try finally block to handle closure of socket on termination

--- server.py
+++ server.py
@@ -9,24 +9,27 @@ s.bind(('',PORT))
 received_indexes_list = []  # list to hold all the indexes we already received
 
 # main script loop
-while True:
-    received_bool = False  # flag to know whether the client sent something
-    while not received_bool:
-        # try to recieve a datagram from the server
-        try:
-            data,addr = s.recvfrom(BUFFER)
-            #split the data using the delimiter to separate the index and the data
-            data_decoded = data.decode('utf8').split('_')
-            index = data_decoded[0]
-            data = data_decoded[1]
-            # if we didn't receive this data before, the index is not in our list, we print the data
-            if index not in received_indexes_list:
-                print(data,end = '')
-                # add the index to the list
-                received_indexes_list.append(index)
-            received_bool = True
-            # send the index to the client to notify we got the message
-            s.sendto(index.encode(),addr)
-        # if no data was received, go to the next iteration
-        except:
-            continue
+try:
+    while True:
+        received_bool = False  # flag to know whether the client sent something
+        while not received_bool:
+            # try to recieve a datagram from the server
+            try:
+                data,addr = s.recvfrom(BUFFER)
+                #split the data using the delimiter to separate the index and the data
+                data_decoded = data.decode('utf8').split('_')
+                index = data_decoded[0]
+                data = data_decoded[1]
+                # if we didn't receive this data before, the index is not in our list, we print the data
+                if index not in received_indexes_list:
+                    print(data,end = '')
+                    # add the index to the list
+                    received_indexes_list.append(index)
+                received_bool = True
+                # send the index to the client to notify we got the message
+                s.sendto(index.encode(),addr)
+            # if no data was received, go to the next iteration
+            except:
+                continue
+finally:
+    s.close()
