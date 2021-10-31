de52332455f3ce657737815bb6d1924c5f518261
funke <funkey7dan@gmail.com>
2021-10-31 17:17:53 +0200

    Added try finally in client

--- client.py
+++ client.py
@@ -12,25 +12,27 @@ s.settimeout(1)
 bytes = file.read(BUFFER)
 index = 0
 # while we have chunks left in the file
-while bytes:
-    # send the chunk to the server, while appending it's index
-    s.sendto(str(index).encode()+b'_'+bytes,(IP,PORT))
-    received_bool = False # flag to know whether the server replies
-    # while we have no answer from the server
-    while not received_bool:
-        # try to recieve a datagram from the server
-        try:
-            data,addr = s.recvfrom(BUFFER)
-            # if we received a datagram, check if it's the index of the chunk we send, as an ACK from server
-            if data.decode() == str(index):
-                received_bool = True
-                # increment index count and read next chunk
-                index += 1
-                bytes = file.read(BUFFER)
-        # if we didn't receive data from the server, send it again
-        except:
-            s.sendto(str(index).encode()+b'_'+bytes,(IP,PORT))
-file.close()
-#close the socket
-s.close()
-exit(0)
+try:
+    while bytes:
+        # send the chunk to the server, while appending it's index
+        s.sendto(str(index).encode()+b'_'+bytes,(IP,PORT))
+        received_bool = False # flag to know whether the server replies
+        # while we have no answer from the server
+        while not received_bool:
+            # try to recieve a datagram from the server
+            try:
+                data,addr = s.recvfrom(BUFFER)
+                # if we received a datagram, check if it's the index of the chunk we send, as an ACK from server
+                if data.decode() == str(index):
+                    received_bool = True
+                    # increment index count and read next chunk
+                    index += 1
+                    bytes = file.read(BUFFER)
+            # if we didn't receive data from the server, send it again
+            except:
+                s.sendto(str(index).encode()+b'_'+bytes,(IP,PORT))
+finally:
+    file.close()
+    #close the socket
+    s.close()
+    exit(0)
